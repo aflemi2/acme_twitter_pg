c128875de9e5f3453886ec805786087d35a2fd22
Alan Fleming <alalfleming@gmail.com>
2018-01-21 16:49:29 -0500

    nodemon script

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+
+app.use(function (req, res, next) {
+  console.log(req.method, req.originalUrl)
+  next()
+})
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(3000, ()=> console.log('server listening'))
