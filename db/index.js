d518d78e9f745981fe140fd672df38e38f896d34
Alan Fleming <alalfleming@gmail.com>
2018-01-29 22:52:43 -0500

    db access thru browser

--- db/index.js
+++ db/index.js
@@ -12,16 +12,45 @@ const client = new pg.Client(process.env.DATABASE_URL);
 client.connect()
 
 const SQL_SYNC = `
+  DROP TABLE IF EXISTS users;
   CREATE TABLE users(
     id SERIAL PRIMARY KEY,
-    name foo
+    name varchar(255),
+    tweet text NOT NULL DEFAULT 'tweet tweet tweet.'::text
   )
+`;
+
+const SQL_SEED = `
+  INSERT INTO users(name, tweet) values ('foo', 'foo tweeting!');
+  INSERT INTO users(name, tweet) values ('baz', 'bar tweeting!');
+  INSERT INTO users(name, tweet) values ('bar', 'baz tweeting!');
 `;
 
 const sync = (cb)=> {
   client.query(SQL_SYNC, cb);
 };
 
+const seed = (cb)=> {
+  client.query(SQL_SEED, cb);
+};
+
+const getTweets = (cb)=> {
+  client.query('SELECT * from users', (err, result)=>{
+    if (err) return cb(err);
+    cb(null, result.rows);
+  });
+};
+
+const getTweet = (id, cb)=> {
+  client.query('SELECT * from users WHERE id = $1', [id], (err, result)=>{
+    if (err) return cb(err);
+    cb(null, result.rows.length ? result.rows[0] : null);
+  });
+};
+
 module.exports = {
-  sync
+  sync,
+  seed,
+  getTweets,
+  getTweet
 };
--- server.js
+++ server.js
@@ -11,18 +11,44 @@ const port = 3000 || process.env.PORT;
 
 db.sync((err)=>{
   if(err) return console.log(err);
-  console.log('table created.');
+  db.getTweets((err, users)=> {
+    if(err) return console.log(err);
+    console.log(`there are ${users.length} users`);
+    db.seed ((err)=> {
+      if(err) return console.log(err);
+      console.log('seed working.');
+      db.getTweets((err, users)=> {
+        if(err) return console.log(err);
+        console.log(`there are ${users.length} users`);
+        db.getTweet(2, (err, user)=> {
+          if(err) return console.log(err);
+          console.log(`user with an id of 2 is ${user.name}`);
+        });
+      });
+    });
+  });
 });
 
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 
- app.get ('/', (req, res, next)=> {
+app.get ('/', (req, res, next)=> {
  res.render('index', { title: 'Twitter' });
 });
 
-  app.get ('/tweets', (req, res, next)=> {
- res.render('tweets', { title: 'Tweet Bank' });
+app.get ('/tweets', (req, res, next)=> {
+ db.getTweets((err, users)=> {
+  if(err) return next(err);
+  res.send(users);
+  //res.render('tweets', { title: 'Tweet Bank' });
+ });
+});
+
+app.get ('/tweets/:id', (req, res, next)=> {
+  db.getTweet(req.params.id, (err, result)=> {
+    if(err) return next (err);
+    res.send(result);
+  });
 });
 
 app.listen(port, ()=>{
